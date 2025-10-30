a5f525582f9d2f574de5192e2bc359ea35af1f98
joaovitorxc <jaovitorxc@gmail.com>
2025-10-30 16:59:05 -0300

    autenticação de login

--- app.py
+++ app.py
@@ -36,37 +36,45 @@ def home():
     return render_template("PaginaHome.html")
 
 @app.route("/apoiadores")
+@login_required
 def apoiadores():
     return render_template("PaginaApoiadores.html")
 
 @app.route("/configuracoes")
+@login_required
 def configuracoes():
     return render_template("PaginaConfig.html")
 
 
 # ----------------- Exercícios -----------------
 @app.route("/exercicios")
+@login_required
 def exercicios():
     return render_template("PaginaExercicios.html")
 
 @app.route("/exercicio-lacunas")
+@login_required
 def exercicio_lacunas():
     return render_template("PaginaLacunas.html")
 
 @app.route("/exercicio-erros")
+@login_required
 def exercicio_erros():
     return render_template("PaginaCorrecaoErros.html")
 
 @app.route("/exercicio-escuta")
+@login_required
 def exercicio_escuta():
     return render_template("PaginaEscuta.html")
 
 # ----------------- Jogos -----------------
 @app.route("/jogo-memoria")
+@login_required
 def jogo_memoria():
     return render_template("PaginaJogoMemoria.html")
 
 @app.route("/jogo-formar-frase")
+@login_required
 def jogo_formar_frase():
     return render_template("PaginaJogoFormarFrase.html")
 
